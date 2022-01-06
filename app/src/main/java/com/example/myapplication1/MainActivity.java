e2d3017ed1e7c3a33fef88e5a46f8e27313a65af
Chandra Prakash <chandraprakash@Chandras-MacBook-Air.local>
2022-01-06 18:51:50 +0530

    changed name

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -3,15 +3,13 @@ package com.example.myapplication1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Handler handler = new Handler();
+        setContentView(R.layout.relative_layout);
     }
+
 }
